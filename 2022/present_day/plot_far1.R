0a6c0bb1ee2684a6994a94d6d8631ca1061e3f25
Chi-Kuang <chi-kuang.yeh@uwaterloo.ca>
2022-04-04 17:02:44 -0400

    Code cleanup

--- 2022/present_day/plot_far1.R
+++ 2022/present_day/plot_far1.R
@@ -6,20 +6,23 @@ library(pbapply)
 library(tibble)
 library(tidyr)
 source("./util.R")
-theme_set(theme_bw())
-theme_update(plot.title = element_text(hjust = 0.5))
 N <- 1000
 Nt <- 101
 H <- 30
 alpha <- 0.05
 s <- seq(0, 1, length.out = Nt)
+ker <- function(tt, s){
+  exp( -(tt^2 + s^2)/2)
+}
+norm_const <- calc_ker_norm_const(-0.2, ker = ker)
+
 
 # Simulate data
 
 # par(mfrow = c(1, 1))
 set.seed(888)
 H <- 30
-my_coef <- -1.1
+
 burn_in <- 50
 N1 <- N + burn_in
 tt <- s <- seq(0, 1, length.out = Nt)
@@ -27,13 +30,14 @@ Z <- rnorm(2)
 my_sin <- sin(2*pi*s)
 my_cos <- cos(2*pi*s)
 x <- Z[1] * sqrt(2) * my_sin + Z[2] * sqrt(2) * my_cos
+x1 <- rep(0, Nt)
 X <- matrix(0, nrow = N1, ncol = Nt)
 X[1, ] <- x
 for(i in 2:N1){
   err_t <- as.numeric(sde::BBridge())
   
-  x1 <- rep(0, Nt)
-  x1 <-  my_coef[1]/(Nt -1) * sum(exp(-s^2/2) * X[i-1,]) * exp(-tt^2/2)
+  
+  x1 <- norm_const/(Nt - 1) * sum(exp(-s^2/2)*X[i-1,])  *exp(-s^2/2)
   X[i, ] <- x1 + err_t
 }
 
@@ -49,11 +53,7 @@ names(df_obs) <-  c("time", paste0("obs_", 1:50))
 df_res <- pivot_longer(df_obs, !time, 
                        names_to = "obs", values_to = "val")
 ggplot(df_res, aes(x = time, y = val, col = obs)) +
-  geom_line() +
-  theme(legend.position = "none") + xlab("Time (t)") + ylab("") +
-  ggtitle("") +
-  theme(plot.title = element_text(hjust = 0.5)) +
-  ggtitle("FAR(1)")
+  geom_line() + labs(x = "Time (t)", y = "")
 
 # FACF
 FACF <- obtain_FACF(Y = obs, v = s, nlags = H, ci = 1 - alpha,
@@ -70,7 +70,6 @@ ggplot(data = df_facf, mapping = aes(x = h, y = rho)) +
   ggtitle("FACF of FAR(1)")
 
 # DACF
-
 df_dacf <- my_new_receipt(obs, H) %>% 
   mutate(lb = qnorm(alpha/2) * std_0_cen/sqrt(N), 
          ub = qnorm(1 - alpha/2) * std_0_cen/sqrt(N))
